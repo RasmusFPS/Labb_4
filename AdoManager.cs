c361b7aa6ffc91f53f7281225b8e28df41eede57
Rasmus <Rasmus.andersen05@gmail.com>
2026-02-07 01:02:14 +0100

    fixed some bugs

--- AdoManager.cs
+++ AdoManager.cs
@@ -15,6 +15,7 @@ namespace Labb_4
     {
         private static readonly string _connectionString = @"Server = localhost;Database = Labb_4;Integrated Security = true;Trust Server Certificate = true;";
 
+        // This one is already using the loop via ExecQuery
         internal static void PrintTeachers()
         {
             Console.Clear();
@@ -30,7 +31,6 @@ namespace Labb_4
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-
                 var command = new SqlCommand(query, connection);
                 try
                 {
@@ -40,7 +40,7 @@ namespace Labb_4
                         {
                             for (int i = 0; i < reader.FieldCount; i++)
                             {
-                                Console.WriteLine($"{reader.GetName(i)} {reader.GetValue(i)}");
+                                Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
                             }
                             Console.WriteLine("-------------------------------------");
                         }
@@ -48,65 +48,9 @@ namespace Labb_4
                 }
                 catch (Exception ex) { Console.WriteLine(ex.Message); }
             }
-
             Console.WriteLine("\nPress AnyKey to Continue");
             Console.ReadKey();
         }
-
-        internal static void NewStaff(string FName, string LName, int rId, int dId, Decimal Salary)
-        {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                string query = "INSERT INTO Staff(FirstName, LastName, RoleID, DepartmentID, Salary, HireDate) VALUES (@FirstName,@LastName,@RoleID,@DepartmentID,@Salary,GETDATE())";
-
-                using (var command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@FirstName", FName);
-                    command.Parameters.AddWithValue("@LastName", LName);
-                    command.Parameters.AddWithValue("@RoleID", rId);
-                    command.Parameters.AddWithValue("@DepartmentID", dId);
-                    command.Parameters.AddWithValue("@Salary", Salary);
-                    try
-                    {
-                        connection.Open();
-
-                        Console.WriteLine($"Changes {command.ExecuteNonQuery()}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-
-                    connection.Close();
-
-                    Console.WriteLine("Press AnyKey to Continue");
-                    Console.ReadKey();
-                }
-            }
-
-        }
-
-        internal static void SaveStaffPrompt()
-        {
-            Console.Write("First Name: "); string fName = Console.ReadLine();
-            Console.Write("Last Name: "); string lName = Console.ReadLine();
-            Console.Write("Role ID (1:Teacher, 2:Principal, 3:Admin): "); int rId = int.Parse(Console.ReadLine());
-            Console.Write("Department ID (1-4): "); int dId = int.Parse(Console.ReadLine());
-            if (dId > 4 && dId > 0)
-            {
-                Console.WriteLine("invalid");
-                return;
-
-            }
-            Console.Write("Salary: "); decimal salary = decimal.Parse(Console.ReadLine());
-            if (salary <= 0 && salary >= 100000)
-            {
-                Console.WriteLine("invalid");
-                salary = 0;
-            }
-            NewStaff(fName, lName, rId, dId, salary);
-        }
-
 
         internal static void GetStudentGrades(int studentId)
         {
@@ -132,40 +76,30 @@ namespace Labb_4
                     using (var command = new SqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@studentId", studentId);
-
                         using (var reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                Console.WriteLine("NAME: " + reader["StudentName"]);
-                                Console.WriteLine("COURSE: " + reader["SubjectName"]);
-                                Console.WriteLine("GRADE: " + reader["Grade"]);
-                                Console.WriteLine("TEACHER " + reader["TeacherName"]);
-                                Console.WriteLine("GRADE DATE " + reader["GradeDate"]);
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
+                                }
                                 Console.WriteLine("---------------------------");
                             }
                         }
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message); 
-                }
-                connection.Close();
-
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
                 Console.WriteLine("Press AnyKey to Continue");
                 Console.ReadKey();
             }
-    
         }
 
         internal static void GetSalary()
         {
             Console.Clear();
-
-            Console.WriteLine("----Salary----");
-
-            string query = "SELECT DepartmentName,SUM(Salary) as Total FROM Staff s JOIN Departments d on s.DepartmentID = d.DepartmentID GROUP BY DepartmentName";
+            Console.WriteLine("----Salary per Department----");
+            string query = "SELECT DepartmentName, SUM(Salary) as Total FROM Staff s JOIN Departments d on s.DepartmentID = d.DepartmentID GROUP BY DepartmentName";
 
             using (var connection = new SqlConnection(_connectionString))
             {
@@ -174,13 +108,18 @@ namespace Labb_4
                     connection.Open();
                     using (var reader = new SqlCommand(query, connection).ExecuteReader())
                     {
-                        while (reader.Read()) Console.WriteLine($"Department {reader["DepartmentName"]}: {reader["Total"]:c} /month");
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
+                            }
+                            Console.WriteLine("---------------------------");
+                        }
                     }
-
                 }
                 catch (Exception ex) { Console.WriteLine(ex.Message); }
             }
-
             Console.Write("\nPress AnyKey to Continue");
             Console.ReadKey();
         }
@@ -188,10 +127,8 @@ namespace Labb_4
         internal static void AvgSalary()
         {
             Console.Clear();
-
-            Console.WriteLine("----Salary----");
-
-            string query = "SELECT DepartmentName,AVG(Salary) as Avgsal FROM Staff s JOIN Departments d on s.DepartmentID = d.DepartmentID GROUP BY DepartmentName";
+            Console.WriteLine("----Average Salary per Department----");
+            string query = "SELECT DepartmentName, AVG(Salary) as AverageSalary FROM Staff s JOIN Departments d on s.DepartmentID = d.DepartmentID GROUP BY DepartmentName";
 
             using (var connection = new SqlConnection(_connectionString))
             {
@@ -200,13 +137,18 @@ namespace Labb_4
                     connection.Open();
                     using (var reader = new SqlCommand(query, connection).ExecuteReader())
                     {
-                        while (reader.Read()) Console.WriteLine($"Department {reader["DepartmentName"]}: {reader["Avgsal"]:c} Avg");
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
+                            }
+                            Console.WriteLine("---------------------------");
+                        }
                     }
-
                 }
                 catch (Exception ex) { Console.WriteLine(ex.Message); }
             }
-
             Console.Write("\nPress AnyKey to Continue");
             Console.ReadKey();
         }
@@ -214,15 +156,14 @@ namespace Labb_4
         internal static void StudentDetails(int studentid)
         {
             Console.Clear();
-            Console.WriteLine("----Student Info----");
+            Console.WriteLine("----Student Info (Stored Procedure)----");
 
-            using(var connection = new SqlConnection(_connectionString))
+            using (var connection = new SqlConnection(_connectionString))
             {
                 using (var command = new SqlCommand("GetStudentDetails", connection))
                 {
                     command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.AddWithValue("@Id", studentid);
-
                     try
                     {
                         connection.Open();
@@ -230,10 +171,11 @@ namespace Labb_4
                         {
                             if (reader.Read())
                             {
-                                Console.WriteLine($"NAME {reader["StudentName"]}");
-                                Console.WriteLine($"SSN {reader["SSN"]}");
-                                Console.WriteLine($"CLASS {reader["ClassName"]}");
-                                Console.WriteLine($"MENTOR {reader["TeacherName"]}");
+                                // LOOPING THROUGH ALL COLUMNS (DYNAMIC)
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
+                                }
                             }
                             else
                             {
@@ -241,26 +183,44 @@ namespace Labb_4
                             }
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
                 }
             }
-
             Console.WriteLine("\nPress AnyKey to Continue");
+            Console.ReadKey();
+        }
+
+        internal static void NewStaff(string FName, string LName, int rId, int dId, Decimal Salary)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Staff(FirstName, LastName, RoleID, DepartmentID, Salary, HireDate) VALUES (@FirstName,@LastName,@RoleID,@DepartmentID,@Salary,GETDATE())";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", FName);
+                    command.Parameters.AddWithValue("@LastName", LName);
+                    command.Parameters.AddWithValue("@RoleID", rId);
+                    command.Parameters.AddWithValue("@DepartmentID", dId);
+                    command.Parameters.AddWithValue("@Salary", Salary);
+                    try
+                    {
+                        connection.Open();
+                        Console.WriteLine($"Rows affected: {command.ExecuteNonQuery()}");
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                }
+            }
+            Console.WriteLine("Press AnyKey to Continue");
             Console.ReadKey();
         }
 
         internal static void SetGrade(int studentId, int teacherId, int subjectId, string grade)
         {
             Console.Clear();
-            Console.WriteLine("----Set Grade----");
-
+            Console.WriteLine("----Set Grade (Transaction)----");
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-
                 using (SqlTransaction transaction = connection.BeginTransaction())
                 {
                     string query = @"INSERT INTO Grades (StudentID, TeacherID, SubjectID, Grade, GradeDate) 
@@ -272,27 +232,22 @@ namespace Labb_4
                         command.Parameters.AddWithValue("@tid", teacherId);
                         command.Parameters.AddWithValue("@sub", subjectId);
                         command.Parameters.AddWithValue("@grade", grade);
-
                         try
                         {
                             command.ExecuteNonQuery();
-
                             transaction.Commit();
-                            Console.WriteLine("Success");
+                            Console.WriteLine("Success Grade saved");
                         }
                         catch (Exception ex)
                         {
                             transaction.Rollback();
-                            Console.WriteLine("Transaction Failed No data was saved.");
-                            Console.WriteLine("Error Message: " + ex.Message);
+                            Console.WriteLine("Transaction Failed. Error: " + ex.Message);
                         }
                     }
                 }
             }
-
             Console.WriteLine("\nPress AnyKey to Continue");
             Console.ReadKey();
         }
     }
-
 }
